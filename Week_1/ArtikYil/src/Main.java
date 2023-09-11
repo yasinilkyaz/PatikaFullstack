66539492df8775802a92459e4044346e1d2c96b2
yasinilkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2023-09-11 22:32:20 +0300

    Artık yıl teslimi

---
+++ Week_1/ArtikYil/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+//Kullanıcıdan yıl bilgisi alındı
+
+        System.out.print("Yıl Giriniz : ");
+        int year = scanner.nextInt();
+
+//Koşullar artık yıl olabilmesi için 4 veya 400'e tam bölünmesi.
+
+        if (year % 4 == 0 && year % 400 == 0) {
+            System.out.println(year + " bir artık yıldır.");
+        } else {
+            System.out.println(year + " bir artık yıl değildir");
+        }
+
+
+    }
+}
