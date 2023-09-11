cb327ea21d394a1cea06f59a550bf0d9da2c7f18
yasinilkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2023-09-11 22:33:49 +0300

    Update Main.java

--- Week_1/ArtikYil/src/Main.java
+++ Week_1/ArtikYil/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
 //Kullanıcıdan yıl bilgisi alındı
@@ -15,7 +15,7 @@ public class Main {
         if (year % 4 == 0 && year % 400 == 0) {
             System.out.println(year + " bir artık yıldır.");
         } else {
-            System.out.println(year + " bir artık yıl değildir");
+            System.out.println(year + " bir artık yıl değildir.");
         }
 
 
