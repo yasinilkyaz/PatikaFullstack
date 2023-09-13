2d532c8029b367f45d282aef0154b2619131472b
yasinilkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2023-09-13 16:37:46 +0300

    asal hafta 2

--- Week_2/AsalSayiV2/src/Main.java
+++ Week_2/AsalSayiV2/src/Main.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    static boolean sayiAsalMi(int sayi,int bolen){
+    static boolean sayiAsalMi(int sayi, int bolen) {
 
-        if (sayi==bolen){return true;}
+        if (sayi == bolen) {
+            return true;
+        }
 
-        if(sayi%bolen==0){
+        if (sayi % bolen == 0) {
             return false;
-        }else {
-            return sayiAsalMi(sayi,bolen+1);
+        } else {
+            return sayiAsalMi(sayi, bolen + 1);
         }
 
     }
 
     public static void main(String[] args) {
 
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Sayı Giriniz : ");
 
-        int sayi=input.nextInt();
+        int sayi = input.nextInt();
 
-        System.out.println(sayi+" sayısı "+(sayiAsalMi(sayi,2) ? "ASALDIR !":"ASAL değildir. !"));
+        System.out.println(sayi + " sayısı " + (sayiAsalMi(sayi, 2) ? "ASALDIR !" : "ASAL değildir. !"));
 
     }
 }
