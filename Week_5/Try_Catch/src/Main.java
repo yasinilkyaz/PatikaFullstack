0f6aefddd1d3a8d3120a59c0659423cac35ffc6c
yasinilkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2023-10-03 22:23:46 +0300

    İndex no kllanıcıdan alındı

--- Week_5/Try_Catch/src/Main.java
+++ Week_5/Try_Catch/src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws ArrayCheckException {
         int [] arr= {0,1,2,3,4,5,6,7,8,9};
-
-        int index=10;
+        Scanner input=new Scanner(System.in);
+        int index=input.nextInt();
         try {
             checkArray(arr,index);
             System.out.println("indeks değeri : "+arr[index]);
