1e34ffaf7919c254b04e92d120fa437429a85b3e
Yasin İlkyaz <79060926+yasinilkyaz@users.noreply.github.com>
2024-01-25 16:28:47 +0300

    todo güncellendi

--- Week_20/To-Do-List/js/main.js
+++ Week_20/To-Do-List/js/main.js
@@ -3,46 +3,58 @@ let ulDOM = document.querySelector("#list");
 let buttonDOm = document.querySelector("#liveToastBtn");
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 const data = JSON.parse(localStorage.getItem('items'));
+let errorToast = document.querySelector("#errorToast");
+
+function showToast(message, type) {
+    let toastDOM = document.querySelector("#liveToast");
+    let toastBodyDOM = toastDOM.querySelector(".toast-body");
+
+    toastDOM.classList.remove("success", "error");
+    toastDOM.classList.add(type);
+    toastBodyDOM.textContent = message;
+
+    $(toastDOM).toast('show');
+}
 
 function newElement() {
-    let inputValue = inputDOM.value;
+    let inputValue = inputDOM.value.trim(); // Boşlukları kaldır
 
-    
+    if (inputValue === "") {
+        showToast('Listeye boş ekleme yapamazsınız!', 'error');
+        inputDOM.value = "";
+        return;
+    }
+
     let liDOM = document.createElement("li");
     liDOM.textContent = inputValue;
 
-    
     let spanDOM = document.createElement("span");
-    spanDOM.textContent = "×"; 
+    spanDOM.textContent = "×";
     spanDOM.classList.add("close");
 
-    
     liDOM.appendChild(spanDOM);
-
-    
     ulDOM.appendChild(liDOM);
 
-    
-    localStorage.clear();
     itemsArray.push(inputValue);
     localStorage.setItem('items', JSON.stringify(itemsArray));
 
-    
-    spanDOM.addEventListener('click', function() {
+    spanDOM.addEventListener('click', function () {
         liDOM.style.display = 'none';
     });
+
+    showToast('Listeye eklendi.', 'success');
+    inputDOM.value = "";
 }
 
 function showExistingTasks() {
-    
-        ulDOM.querySelectorAll('li').forEach(function (liDOM) {
+    ulDOM.querySelectorAll('li').forEach(function (liDOM) {
         let spanDOM = document.createElement("span");
         spanDOM.textContent = "×";
         spanDOM.classList.add("close");
 
         liDOM.appendChild(spanDOM);
 
-        spanDOM.addEventListener('click', function() {
+        spanDOM.addEventListener('click', function () {
             liDOM.style.display = 'none';
         });
     });
@@ -50,16 +62,12 @@ function showExistingTasks() {
 
 showExistingTasks();
 
-
-ulDOM.addEventListener('click', function(event) {
+ulDOM.addEventListener('click', function (event) {
     const clickedItem = event.target;
 
-    
     if (clickedItem.tagName === 'LI') {
-        
         const isSelected = clickedItem.classList.contains("checked");
 
-        
         if (isSelected) {
             clickedItem.classList.remove("checked");
         } else {
